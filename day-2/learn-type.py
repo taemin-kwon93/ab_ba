db793fd5661359d5b873016cfb154d301e02c59a
taemin-kwon93 <4146kt@gmail.com>
2023-05-05 13:31:37 +0900

    chore: type

--- day-2/learn-type.py
+++ day-2/learn-type.py
@@ -1,6 +1,10 @@
 two_digit_number = input("Type a two digit number: ")
+# Type a two digit number: 30
+
 print(type(two_digit_number)) # <class 'str'>
 print(type(int(two_digit_number))) # <class 'int'>
 
-# console
-# Type a two digit number: 30
+first_digit = two_digit_number[0]
+second_digit = two_digit_number[1]
+print(type(first_digit)) # <class 'str'>
+print(second_digit) # 0
