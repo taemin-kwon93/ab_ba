88779bf0a7b79a89a8e86e668fa4f16c95501e53
taemin-kwon93 <4146kt@gmail.com>
2023-07-08 16:01:57 +0900

    study

---
+++ code-up-100/print-input/next-character.py
@@ -0,0 +1,16 @@
+def main():
+    character = input()
+    print(chr(ord(character) + 1))
+
+
+main()
+
+# def main():
+#     character = input()
+#     print(chr(ord(character) + 1))
+#     # 설명 : (ord(character)를 실행하면 character의 아스키 코드값을 반환한다.
+#     # ord(character) + 1을 실행하면 character의 아스키 코드값에 1을 더한 값을 반환한다.
+#     # chr(ord(character) + 1)을 실행하면 character의 아스키 코드값에 1을 더한 값을 문자로 반환한다.
+#
+#
+# main()
--- code-up-100/print-input/reverse-sign.py
+++ code-up-100/print-input/reverse-sign.py
@@ -1,1 +1,13 @@
+def main():
+    x = int(input())
+    print(-x)
 
+
+def throwError():
+    print('have to input integer')
+
+
+try:
+    main()
+except:
+    throwError()
